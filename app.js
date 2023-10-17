d04e2f05e3e3164e18eec262cd5a76b4c1dab0ce
sumit <itssumit258@gmail.com>
2023-10-17 18:27:53 +0530

    feat. category image updated

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ APP_DATA.CATEGORIES.map((category) => {
     ` <div class="flex_col gap_sm category_item self_center" id="category">
     <a href=${url}>
         <div class="items_center category_box self_center">
-        <span class="category_text items_center self_center text-sm">${category}</span>
+        <span class="category_text items_center self_center text_lg">${category}</span>
         </div>
 
     </a>
