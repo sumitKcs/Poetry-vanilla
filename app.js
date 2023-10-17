40931d24613388fd1b23e88fae2167142b8435e9
sumit <itssumit258@gmail.com>
2023-10-17 13:55:48 +0530

    feat. dynamic slider images

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import APP_DATA from "./data.js";
 
 const navigation = document.getElementById("main_navigation");
 const fragment = document.createDocumentFragment();
+const slider = document.getElementById("slider");
 
 let list = "";
 APP_DATA.MENU.map((item) => {
@@ -10,4 +11,14 @@ APP_DATA.MENU.map((item) => {
 
 navigation.innerHTML = list;
 
-
+APP_DATA.SLIDER_IMAGES.map((image) => {
+  const div = document.createElement("div");
+  div.classList.add("swiper-slide");
+  const img = document.createElement("img");
+  img.src = image;
+  div.appendChild(img);
+  fragment.appendChild(div);
+});
+slider.innerHTML = "";
+slider.appendChild(fragment);
+fragment.innerHTML = "";
